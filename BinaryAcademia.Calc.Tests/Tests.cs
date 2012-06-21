5fcdafa2a6299fd01a281bca2401cfe89ffbaf10
Nikita Exaurov <exaurov.nm@mail.ru>
2012-06-21 17:49:02 +0300

    New Names
    Changed names of Tests.

--- BinaryAcademia.Calc.Tests/Tests.cs
+++ BinaryAcademia.Calc.Tests/Tests.cs
@@ -6,28 +6,28 @@ namespace BinaryAcademia.Calc.Tests
     class Tests
     {   
         [Test]
-        public void Should_EqualeToSum()
+        public void Should_Equale3_WhenAdd1To2()
         {
             var calc = new Calc();
             Assert.That(calc.Add(1,2),Is.EqualTo(3));
         }
 
         [Test]
-        public void Should_EqualeToProduct()
+        public void Should_Equale15_When3Multby5()
         {
             var calc = new Calc();
             Assert.That(calc.Multiplication(3,5),Is.EqualTo(15));
         }
 
         [Test]
-        public void Slould_EqualeToDifference()
+        public void Slould_Equale5_WhenDo10Minus5()
         {
             var calc = new Calc();
             Assert.That(calc.Difference(10,5),Is.EqualTo(5));
         }
 
         [Test]
-        public void Should_EqualeToQuotient()
+        public void Should_Equale5_When30DivBy6()
         {
             var calc = new Calc();
             Assert.That(calc.Division(30, 6), Is.EqualTo(5));
